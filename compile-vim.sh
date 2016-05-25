717fa47b87b555f49aa296b36191ceac8a3f0e2d
Conan Li <conan.li@juicemobile.com>
2016-05-25 21:19:10 +0000

    : download to /tmp

--- compile-vim.sh
+++ compile-vim.sh
@@ -6,11 +6,9 @@ VIM_VERSION=${VIM_VERSION:-7.4.898}
 (vi --version | grep '+python3') && echo Already support python 3. Will not compile. && exit 0
 sudo apt-get update && sudo apt-get install -y unzip ncurses-dev libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
 
-wget https://github.com/vim/vim/archive/v7.4.898.zip -O ${HOME}/${VIM_VERSION}.zip
+wget https://github.com/vim/vim/archive/v7.4.898.zip -O /tmp/${VIM_VERSION}.zip
 
-cd ${HOME} && \
-    unzip ${VIM_VERSION} && \
-    cd ${VIM_VERSION} && \
+cd /tmp && unzip ${VIM_VERSION} && cd ${VIM_VERSION} && \
     ./configure --with-features=huge --enable-python3interp=dynamic --enable-cscope --prefix=${HOME} && \
     make && \
     make install && \
