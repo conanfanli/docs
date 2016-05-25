cfee39309b9000486be4b65a663fdb61ddc4df1d
Conan Li <conan.li@juicemobile.com>
2016-05-25 21:33:45 +0000

    : dir name

--- compile-vim.sh
+++ compile-vim.sh
@@ -8,10 +8,8 @@ sudo apt-get update && sudo apt-get install -y unzip ncurses-dev libx11-dev libx
 
 wget https://github.com/vim/vim/archive/v7.4.898.zip -O /tmp/${VIM_VERSION}.zip
 
-cd /tmp && unzip ${VIM_VERSION} && cd ${VIM_VERSION} && \
+cd /tmp && unzip ${VIM_VERSION} && cd vim-${VIM_VERSION} && \
     ./configure --with-features=huge --enable-python3interp=dynamic --enable-cscope --prefix=${HOME} && \
     make && \
     make install && \
     sudo ln -s ${HOME}/bin/vim /usr/bin/vi -f
-
-exit 0
