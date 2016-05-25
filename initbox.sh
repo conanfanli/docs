9b25fdde2b9fb1b3c9fe02fb2b54f63c67cb20a1
Conan Li <conan.li@juicemobile.com>
2016-05-25 18:52:02 +0000

    : initbox.sh

---
+++ initbox.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+set -e
+
+sudo apt-get update && sudo apt-get -y install curl
+
+curl -L https://bootstrap.saltstack.com | sudo sh -s -- stable
+
+cd /home/ubuntu/
+mkdir -p projects
+cd projects
+
+# Clone iconfigs if not exists
+test -d iconfigs || git clone https://conanfanli@bitbucket.org/conanfanli/iconfigs.git
+
+# Symlink
+sudo ln -s /home/ubuntu/projects/iconfigs /srv/salt/iconfgis
+
+# Change owner
+sudo chown -R ubuntu:ubuntu /home/ubuntu/
+sudo chown -R ubuntu:ubuntu /srv/salt
+
+# Apply salt
+sudo salt-call --local state.apply iconfigs
