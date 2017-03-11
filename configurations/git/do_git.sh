7807943dee9da615281297472506109cc0f4d885
Conan Li <conanlics@gmail.com>
2017-03-11 10:40:38 -0500

    bash completion for git

--- configurations/git/do_git.sh
+++ configurations/git/do_git.sh
@@ -1,7 +1,16 @@
 #!/bin/sh -e
 
-CONFIG_FILE="$HOME/docs/configurations/git/gitconfig"
+GIT_CONFIG_DIR="$HOME/docs/configurations/git"
+COMPLETION_FILE_PATH="$GIT_CONFIG_DIR/git-completion.bash"
+CONFIG_FILE="$GIT_CONFIG_DIR/gitconfig"
 cp $CONFIG_FILE ~/.gitconfig
 
 echo Installed gitconfig
 cat ~/.gitconfig
+
+# ls /etc/bash_completion.d/git*
+if [ -d "/etc/bash_completion.d" ]
+then
+    ls /etc/bash_completion.d/git-* 1> /dev/null 2>&1 && echo git completion already exists || sudo cp $COMPLETION_FILE_PATH /etc/bash_completion.d/
+    ls /etc/bash_completion.d/git-*
+fi
