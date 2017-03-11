474dfdd4b1f40b2d13cc03bc03c2137334ca1bef
Conan Li <conanlics@gmail.com>
2017-03-10 22:38:52 -0500

    remove commented

--- check_sys.sh
+++ check_sys.sh
@@ -26,20 +26,3 @@ then
     echo centos > data/os
     exit 0
 fi
-
-# info=`cat /etc/*-release`
-# echo $info
-# set -e
-# VIM_VERSION=${VIM_VERSION:-7.4.898}
-#
-# (vi --version | grep '+python3') && echo Already support python 3. Will not compile. && exit 0
-# sudo apt-get update && sudo apt-get install -y unzip ncurses-dev libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
-#
-# wget https://github.com/vim/vim/archive/v7.4.898.zip -O /tmp/vim-${VIM_VERSION}.zip
-# cd /tmp
-# test -d vim-${VIM_VERSION} || unzip vim-${VIM_VERSION}
-# cd vim-${VIM_VERSION} && \
-#     ./configure --with-features=huge --enable-python3interp=dynamic --enable-cscope --prefix=${HOME} && \
-#     make && \
-#     make install && \
-#     sudo ln -s ${HOME}/bin/vim /usr/bin/vi -f
