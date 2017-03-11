377bf88a61a89ea5b4296e44653ce926bb746472
Conan Li <conanlics@gmail.com>
2017-03-11 10:43:53 -0500

    mac

--- configurations/git/do_git.sh
+++ configurations/git/do_git.sh
@@ -8,9 +8,17 @@ cp $CONFIG_FILE ~/.gitconfig
 echo Installed gitconfig
 cat ~/.gitconfig
 
-# ls /etc/bash_completion.d/git*
+
+# On Linux
 if [ -d "/etc/bash_completion.d" ]
 then
     ls /etc/bash_completion.d/git-* 1> /dev/null 2>&1 && echo git completion already exists || sudo cp $COMPLETION_FILE_PATH /etc/bash_completion.d/
     ls /etc/bash_completion.d/git-*
+    exit 0
+fi
+
+# On Mac
+if [ -d "/usr/local/etc/bash_completion.d/" ] && [ ! -f "/usr/local/etc/bash_completion.d/git-completion.bash" ]
+then
+    echo 1111111
 fi
