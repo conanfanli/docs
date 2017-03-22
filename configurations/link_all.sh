2f06f978a3c0aa20f6212a2d5992492744b25141
Conan Li <conanlics@gmail.com>
2017-03-21 22:16:25 -0400

    linall

---
+++ configurations/link_all.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -is $DIR/bash/bash_profile.bash ~/.bash_profile
+ln -is $DIR/git/gitconfig ~/.gitconfig
+ln -is $DIR/tmux/tmux.conf ~/.tmux.conf
+ln -is $DIR/vim/vimrc ~/.vimrc
