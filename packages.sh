5d6cb39e20235a7249bc8a58d2bd7296576f032a
Conan Li <conanlics@gmail.com>
2017-03-22 04:12:40 +0000

    add packages.sh

---
+++ packages.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -d ~/.fzf ]
+then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    yes | ~/.fzf/install
+fi
+
+$DIR/check_sys.sh
+
+install_ag () {
+
+    if command -v ag > /dev/null 2>&1;
+    then
+        echo ag is already installed
+        return
+    fi
+
+    os=`cat data/os`
+    if [ "$os" == "centos" ]; then
+        sudo yum install epel-release.noarch the_silver_searcher;
+    elif [ "$os" == "ubuntu" ]; then
+        sudo apt-get install silversearcher-ag;
+    elif [ "$os" == "mac" ]; then
+        brew install the_silver_searcher;
+    else
+        echo not supported OS $os;
+        exit 1;
+    fi
+}
+
+install_ag
+
