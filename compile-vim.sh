6b6111ea38827d9e6cb8e6570f3e1b666b9bfbed
Conan Li <conanlics@gmail.com>
2017-05-04 21:20:02 +0000

    vim settings

--- compile-vim.sh
+++ compile-vim.sh
@@ -5,7 +5,7 @@ set -e
 
 
 install-vim () {
-    (vim --version | grep -o 8.0) && echo 'Your Vim is already up to date' && exit 0
+    (vim --version | grep -o 8.0) && echo 'Your Vim is already up to date' && return 0
 
     sudo apt-get update && sudo apt-get install -y make unzip ncurses-dev libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
 
@@ -19,5 +19,12 @@ install-vim () {
         sudo ln -s ${HOME}/bin/vim /usr/bin/vi -f
 }
 
+install-dein () {
+    # test -d ~/repos/github.com/Shougo && echo 'Dein is already installed' && return 0
+    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+    sh ./installer.sh ~/.vim/bundles
+}
+
 
 install-vim
+install-dein
