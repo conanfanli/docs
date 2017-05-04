b5f0750351cb49a5dbb38315bd60351fab33b97c
Conan Li <conanlics@gmail.com>
2017-05-04 20:46:47 +0000

    vim 8

--- compile-vim.sh
+++ compile-vim.sh
@@ -1,16 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e 
-VIM_VERSION=${VIM_VERSION:-7.4.898}
+set -e
 
-(vi --version | grep '+python3') && echo Already support python 3. Will not compile. && exit 0
-sudo apt-get update && sudo apt-get install -y unzip ncurses-dev libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
 
-wget https://github.com/vim/vim/archive/v7.4.898.zip -O /tmp/vim-${VIM_VERSION}.zip
-cd /tmp 
-test -d vim-${VIM_VERSION} || unzip vim-${VIM_VERSION} 
-cd vim-${VIM_VERSION} && \
+
+install-vim () {
+    (vim --version | grep -o 8.0) && echo 'Your Vim is already up to date' && exit 0
+
+    sudo apt-get update && sudo apt-get install -y make unzip ncurses-dev libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
+
+    cd ~
+    test -d vim || git clone https://github.com/vim/vim.git
+
+    cd vim/src && \
     ./configure --with-features=huge --enable-python3interp=dynamic --enable-cscope --prefix=${HOME} && \
-    make && \
-    make install && \
-    sudo ln -s ${HOME}/bin/vim /usr/bin/vi -f
+        make && \
+        make install && \
+        sudo ln -s ${HOME}/bin/vim /usr/bin/vi -f
+}
+
+
+install-vim
