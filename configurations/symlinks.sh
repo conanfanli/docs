e59c1722f4450f58521cf8953b9fdaa67c6a5e60
Conan Li <conanlics@gmail.com>
2017-06-16 16:09:29 -0400

    tmux conf

--- configurations/symlinks.sh
+++ configurations/symlinks.sh
@@ -4,5 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -is $DIR/bash/bash_profile.bash ~/.bash_profile
 ln -is $DIR/git/gitconfig ~/.gitconfig
 ln -is $DIR/tmux/tmux.conf ~/.tmux.conf
+ln -is $DIR/tmux/tmux.conf.local ~/.tmux.conf.local
 ln -is $DIR/vim/vimrc ~/.vimrc
 ln -is $DIR/ag/agignore ~/.agignore
