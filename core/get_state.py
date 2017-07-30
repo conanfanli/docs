59e9f3022908aa1d1fa3d1ce133e7722a3c7b28c
Conan Li <conanlics@gmail.com>
2017-07-30 00:09:31 -0400

    get state

--- core/get_state.py
+++ core/get_state.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+from pprint import pprint
 import subprocess
 import logging
 from command_streamer import stream_command
@@ -5,50 +7,69 @@ from command_streamer import stream_command
 
 logger = logging.getLogger(__name__)
 
-def on_stdout(s):
-    logger.info(s.decode('utf-8'))
 
 class EvalCommand:
 
-    def __init__(self, command: str, shell: bool = False) -> None:
+    def __init__(self, command: str) -> None:
         self.command = command
-        # self.shell = shell
+        self.return_code = None
 
     def to_dict(self):
         return {
             'command': self.command,
-            'result': self.result
+            'return_code': self.return_code
         }
+
+    def __repr__(self):
+        return '{}({})'.format(self.__class__.__name__, repr(self.command))
 
     def evaluate(self):
         try:
-            output = subprocess.check_output(self.command)
-            # rc = stream_command(self.command, on_stdout)
-            # print('return code', rc)
-            logger.info(output)
-            self.result = True
+            rc = stream_command(self.command, on_stdout)
+            self.return_code = True
         except subprocess.CalledProcessError:
-            self.result = False
+            self.return_code = False
             return False
 
 
-state = {
+
+def on_stdout(s):
+    logger.info(s.decode('utf-8'))
+
+
+state_definition = {
     "branchIsUpdated": EvalCommand(
         command=['bash', '-c',
                  'git remote update && git status -uno | grep up-to-date']
     )
 }
 
+fields = sorted(state_definition.keys())
+
+
+AbstractState = namedtuple('AbstractState', fields)
+
+
+class State(AbstractState):
+
+    def evaluate(self):
+        pass
+
+    def to_dict(self):
+        rv = self._asdict()
+        for key in self._fields:
+            value = getattr(self, key)
+            if hasattr(value, 'to_dict'):
+                rv[key] = value.to_dict()
+
+        return rv
+
+
+initial_state = State(**state_definition)
+
 
 def evaluate(state):
     state["branchIsUpdated"].evaluate()
 
 
-def serialize(state):
-    for key in state:
-        value = state[key]
-        if hasattr(value, 'to_dict'):
-            state[key] = value.to_dict()
-
-    return state
-
+pprint(dict(initial_state.to_dict()))
