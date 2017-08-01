1aa1b4ed95daaca823a88c9a8a1e7a7b8f13eead
Conan Li <conanlics@gmail.com>
2017-08-01 11:20:24 -0400

    become

--- compile-vim.sh
+++ compile-vim.sh
@@ -25,7 +25,7 @@ fi
 install-vim () {
     (~/bin/vim --version | grep -o 'IMproved 8.0') && echo 'Your Vim is already up to date' && return 0
 
-    sudo apt-get update && sudo apt-get install -y make unzip ncurses-dev libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
+    apt-get update && apt-get install -y make unzip ncurses-dev libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
 
     cd ~
     test -d vim || git clone https://github.com/vim/vim.git
