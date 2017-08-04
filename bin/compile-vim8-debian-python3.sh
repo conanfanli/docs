04900af4af66f320a25934c8235449dc23ebf18f
Conan Li <conanlics@gmail.com>
2017-08-04 23:19:06 +0000

    exit 0

--- bin/compile-vim8-debian-python3.sh
+++ bin/compile-vim8-debian-python3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-(vim --version | grep -o 'IMproved 8.0') && echo 'Your Vim is already up to date' && return 0
+(vim --version | grep -o 'IMproved 8.0') && echo 'Your Vim is already up to date' && exit 0
 
 apt-get update && apt-get install -y make unzip ncurses-dev libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
 
