f92d7930da7fc4030cbde7398be0d4d956b91313
Conan Li <conanlics@gmail.com>
2017-08-04 16:03:11 -0400

    print

--- bin/get_all_aliases.py
+++ bin/get_all_aliases.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import re
 from os.path import expanduser
 
@@ -6,12 +7,8 @@ FUNCTION_REGEX = re.compile(r'^(\w+) \(\) {.+#desc#: ?(.+)$')
 
 with open(expanduser('~/.rice.bash')) as f:
     for line in f:
-        match = ALIAS_REGEX.match(line)
-        if match:
-            print(match.groups())
-            continue
-
-        match = FUNCTION_REGEX.match(line)
-        if match:
-            print(match.groups())
-            continue
+        for regex in (ALIAS_REGEX, FUNCTION_REGEX):
+            match = regex.match(line)
+            if match:
+                print(': '.join(match.groups()))
+                continue
