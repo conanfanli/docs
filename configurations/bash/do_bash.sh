1bedce7089f9d8a140b92b164605ac4a0cd4d1f0
Conan Li <conanlics@gmail.com>
2017-09-23 14:18:59 +0000

    wip

--- configurations/bash/do_bash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-
-BEGIN='# START bash_profile -DO-NOT-EDIT'
-
-touch ~/.bash_profile
-grep "$BEGIN" ~/.bash_profile || cat ~/rice/configurations/bash/bash_profile.bash >> ~/.bash_profile
