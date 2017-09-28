dcdb0c04300a480c0b5aca826f46c01569d554da
Conan Li <conanlics@gmail.com>
2017-09-28 22:40:03 +0000

    server

--- gonuts/cmd/myapp/main.go
+++ gonuts/cmd/myapp/main.go
@@ -1,90 +1,18 @@
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"encoding/base64"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 	"path"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	SERVER_KEY := os.Getenv("SERVER_KEY")
-
 	first, text := path.Split(r.URL.Path[1:])
-	password := first[:len(first)-1]
-
-	if password != SERVER_KEY {
-		fmt.Println(w, "Bad key!")
-		return
-	}
-
-	key := []byte(SERVER_KEY)
-
-	encrypted := encrypt(key, text)
-	fmt.Println("encrypted: ", encrypted)
-
-	// Decrypt
-	decrypted := decrypt(key, encrypted)
-	fmt.Println("decrypted: ", decrypted)
+	fmt.Fprintln(w, first, text)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Starting server ...")
 	http.ListenAndServe(":3000", nil)
-}
-
-// encrypt string to base64 crypto using AES
-func encrypt(key []byte, text string) string {
-	// key := []byte(keyText)
-	plaintext := []byte(text)
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err)
-	}
-
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
-	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
-	}
-
-	stream := cipher.NewCFBEncrypter(block, iv)
-	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
-
-	// convert to base64
-	return base64.URLEncoding.EncodeToString(ciphertext)
-}
-
-// decrypt from base64 to decrypted string
-func decrypt(key []byte, cryptoText string) string {
-	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err)
-	}
-
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
-	if len(ciphertext) < aes.BlockSize {
-		panic("ciphertext too short")
-	}
-	iv := ciphertext[:aes.BlockSize]
-	ciphertext = ciphertext[aes.BlockSize:]
-
-	stream := cipher.NewCFBDecrypter(block, iv)
-
-	// XORKeyStream can work in-place if the two arguments are the same.
-	stream.XORKeyStream(ciphertext, ciphertext)
-
-	return fmt.Sprintf("%s", ciphertext)
 }
