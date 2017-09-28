40a30abfc1b203523f4c1bc1beeee84893f93016
Conan Li <vagrant@vagrant.vm>
2017-09-28 00:57:44 +0000

    encryp

--- gonuts.go
+++ gonuts.go
@@ -1,15 +1,52 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	text := r.URL.Path[1:]
+	fmt.Println(text)
+	key := []byte("example key 1234")
+
+	encrypted := encrypt(key, text)
+	fmt.Println("encrypted: %s", encrypted)
+	fmt.Fprintf(w, "Hi there, I love %s!", text)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	fmt.Println("Starting server ...")
 	http.ListenAndServe(":8080", nil)
+}
+
+// encrypt string to base64 crypto using AES
+func encrypt(key []byte, text string) string {
+	// key := []byte(keyText)
+	plaintext := []byte(text)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	// convert to base64
+	return base64.URLEncoding.EncodeToString(ciphertext)
 }
