43abbdbf0de8c7f18ac201e9f981f711a09e183b
Conan Li <conanlics@gmail.com>
2017-09-30 00:14:08 -0400

    gin

--- gonuts/cmd/myapp/main.go
+++ gonuts/cmd/myapp/main.go
@@ -6,14 +6,15 @@ import (
 	"strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	urlPath := r.URL.Path[1:]
 	parts := strings.Split(urlPath, "/")
 	fmt.Fprintln(w, parts)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/home/", homeHandler)
+	http.HandleFunc("/store/", homeHandler)
 	fmt.Println("Starting server ...")
 	http.ListenAndServe(":3001", nil)
 }
