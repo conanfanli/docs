dc2a76a844d15b615201662c3896c05ea4f7bfa8
Conan Li <conanlics@gmail.com>
2017-10-26 04:01:52 +0000

    secret

--- commando/drop_tmate.sh
+++ commando/drop_tmate.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
+machine_name=${machine_name-$1}
+secret=${secret}
+
+
+if [ -z "$machine_name" ]; then
+    echo Missing machine name!!!
+    exit 1
+fi
+
+if [ -z "$secret" ]; then
+    echo missing secret variable environment variable
+    exit 1
+fi
+
 tmate -S /tmp/tmate.sock new-session -d
 
 set -e
-cd ~/dropbox/tmates
-git pull
-local_machine_name=`cat ~/dropbox/tmates/local_machine_name`
-tmate -S /tmp/tmate.sock display -p '#{tmate_web}' > ~/dropbox/tmates/$local_machine_name
-git add -A
-git commit -m "drop tmate session $local_machine_name"
-git push
+
+session=`tmate -S /tmp/tmate.sock display -p '#{tmate_web}'`
+
+if [ -z "$session" ]; then
+    echo cannot create tmate session
+    exit 1
+fi
+echo session is $session
+b64=`echo $session | base64`
+url="https://dropping-dimes.herokuapp.com/${secret}=/set/tmate-$machine_name/$b64"
+echo $url
+curl "$url"
