0c3a55e12d5228e01f665c38147e3756bc5325ca
Conan Li <conanlics@gmail.com>
2017-10-26 04:34:21 +0000

    worked

--- commando/create_tmate_session.sh
+++ commando/create_tmate_session.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-machine_name=${machine_name-$1}
-secret=${secret}
+dime_bag_url=${dime_bag_url}
 
 
-if [ -z "$machine_name" ]; then
-    echo Missing machine name!!!
-    exit 1
-fi
-
-if [ -z "$secret" ]; then
-    echo missing secret variable environment variable
+if [ -z "$dime_bag_url" ]; then
+    echo missing dime_bag_url variable environment variable
     exit 1
 fi
 
@@ -26,6 +20,7 @@ if [ -z "$session" ]; then
 fi
 echo session is $session
 b64=`echo $session | base64`
-url="https://dropping-dimes.herokuapp.com/${secret}=/set/tmate-$machine_name/$b64"
+
+url="${dime_bag_url}/$b64"
 echo $url
 curl "$url"
