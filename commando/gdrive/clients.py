f942ffbab9d9d5739f812da5d8dab2659ddc2c34
Conan Li <conanlics@gmail.com>
2017-10-27 21:43:48 -0400

    list_fiels

--- commando/gdrive/clients.py
+++ commando/gdrive/clients.py
@@ -2,6 +2,7 @@
 import httplib2
 import os
 import argparse
+from pprint import pprint
 
 from googleapiclient.discovery import Resource
 from apiclient import discovery
@@ -31,7 +32,6 @@ class GClient:
 
         http = credentials.authorize(httplib2.Http())
         service = discovery.build('drive', 'v3', http=http)
-        print(service)
         return service
 
     @staticmethod
@@ -64,8 +64,14 @@ class GClient:
             print('Storing credentials to ' + credential_path)
         return credentials
 
-    def list_files(self):
-        return self.service.files().list()
+    def list_files(self,
+                   *,
+                   pageSize: int=20,
+                   fields: str='nextPageToken, files(id, name)'
+                   ):
+        return self.service.files().list(
+            pageSize=pageSize
+        ).execute()
 
 
 def main():
@@ -74,17 +80,9 @@ def main():
     Creates a Google Drive API service object and outputs the names and IDs
     for up to 10 files.
     """
-    service = GClient().get_service()
-    results = service.files().list(
-        pageSize=10,
-        fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print('{0} ({1})'.format(item['name'], item['id']))
+    gclient = GClient()
+    results = gclient.list_files()
+    pprint(results)
 
 
 if __name__ == '__main__':
