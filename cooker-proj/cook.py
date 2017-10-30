8344e81d6de095dca61aa81057a72bb3b0bca49f
Conan Li <conanlics@gmail.com>
2017-10-30 11:30:58 -0400

    logging

--- cooker-proj/cook.py
+++ cooker-proj/cook.py
@@ -1,7 +1,52 @@
 #!/usr/bin/env python3.6
+import logging
+import logging.config
 import argparse
 from commandos.printer import print_red, Color
 from commandos.base import Commando
+
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s: %(name)s %(message)s'
+        },
+        # You may need to specify the timezone here.
+        # For example: %(asctime)s CST [%(levelname)s] %(name)s:
+        # %(message)s
+        'standard': {
+            'format': '[%(asctime)s %(levelname)s/%(process)d] [%(module)s]: %(message)s'
+        },
+    },
+
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+
+    'loggers': {
+        'cooker': {
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'django': {
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG'
+    }
+}
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger(__name__)
 
 
 def main():
